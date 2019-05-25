f01798393334f6a1119231fc895c7c6449a10fd9
DESKTOP-J85S2VT\Lenovo <taochenhao@qq.com>
2019-05-25 17:22:29 +0800

    生产版

--- run_confs/stop.sh
+++ run_confs/stop.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PID=$(ps -ef | grep zijida-server-0.0.1-SNAPSHOT.jar | grep -v grep | awk '{ print $2 }')
+PID=$(ps -ef | grep zijida-server-0.0.1-SNAPSHOT-prod.jar | grep -v grep | awk '{ print $2 }')
 if [ -z "$PID" ]
 then
     echo Application is already stopped
